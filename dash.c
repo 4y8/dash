7d467ed7355bf05e6b03d8a853a9c89342dd892f
Yul3n <aghilas.boussaa@laposte.net>
2020-05-21 17:11:39 +0200

    Migrated to SL2.

--- dash.c
+++ dash.c
@@ -1,30 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <SDL/SDL.h>
+#include <SDL2/SDL.h>
+#include <math.h>
 
-int SCREEN_WIDTH = 640;
+int SCREEN_WIDTH  = 640;
 int SCREEN_HEIGHT = 480;
 
 int WHITE = 0xFFFFFF, BLACK = 0x000000;
 
-SDL_Surface *screen;
+SDL_Window   *screen;
+SDL_Renderer *renderer;
+
+typedef struct Vector {
+	int x;
+	int y;
+} vector;
 
 int player_x = 0, player_y = 0;
+vector player_speed;
+
+vector
+get_mouse(vector v)
+{
+	SDL_Event e;
+
+	while (SDL_PollEvent(&e) && (e.type == SDL_MOUSEMOTION)) {
+		int ny = e.motion.y - player_y;
+		printf("%d\n%d\n", e.motion.x, e.motion.y);
+		if (ny == 0) { 
+			v.y = 0;
+			v.x = 5 * floor((e.motion.x - player_x));
+		}
+		else { 
+			v.y = 5 * ny / abs(ny);
+			v.x = 5 * floor((e.motion.x - player_x) / (abs(ny)));
+		}
+	}
+	return v;
+}
 
 void
 draw_rectangle (int x, int y, int w, int h, int color)
 {
-	SDL_Rect pos;
-	pos.x = x;
-	pos.y = y;
-	SDL_Surface *rect = SDL_CreateRGBSurface(SDL_HWSURFACE, w, h, 24, 0, 0, 0, 0);
-	SDL_FillRect(rect, NULL, SDL_MapRGB
-				 (screen->format,
-				  color & 0xFF0000 >> 16,
-				  color & 0x00FF00 >> 8,
-				  color & 0x0000FF));
-	SDL_BlitSurface(rect, NULL, screen, &pos);
-	SDL_Flip(screen);
+	SDL_Rect rect;
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer,
+						   color & 0xFF0000 >> 16,
+						   color & 0x00FF00 >> 8,
+						   color & 0x0000FF,
+						   255);
+	rect.x = x;
+	rect.y = y;
+	rect.w = w;
+	rect.h = h;
+	SDL_RenderFillRect(renderer, &rect);
+	SDL_RenderPresent(renderer);
 }
 
 void
@@ -33,19 +65,32 @@ update_player(int x, int y)
 	draw_rectangle(player_x, player_y, 10, 10, BLACK);
 	player_x = x;
 	player_y = y;
-	draw_rectangle(x, y, 10, 10, WHITE);
+	draw_rectangle(player_x, player_y, 10, 10, WHITE);
 
+}
+
+void
+init()
+{
+	player_speed.x = 0;
+	player_speed.y = 0;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	screen = SDL_CreateWindow("dash", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	renderer = SDL_CreateRenderer(screen, -1, 0);
+	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 }
 
 int
 main()
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 24, SDL_HWSURFACE);
-	draw_rectangle(0, 0, 222, 222, WHITE);
-	draw_rectangle(30, 30, 222, 222, WHITE);
+	init();
+	while (1) {
+		player_speed = get_mouse(player_speed);
+		update_player(player_x + player_speed.x, player_y + player_speed.y);
+		SDL_Delay(100);
+	}
 	SDL_Delay(3000);
-	SDL_FreeSurface(screen);
 	SDL_Quit();
 	return 0;
 }
